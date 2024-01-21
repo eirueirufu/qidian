970b189766ca95f2f7005e027d509782692db222
eirueirufu <57451255+eirueirufu@users.noreply.github.com>
2024-01-21 21:08:21 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:qidian/app/app.dart';
 
 void main() {
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.dark,
